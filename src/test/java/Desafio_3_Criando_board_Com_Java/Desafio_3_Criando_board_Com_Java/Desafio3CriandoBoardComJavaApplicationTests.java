20ad85edf708851b07c662c0e31dbdadbb956803
Matheus-120 <mathbaf@gmail.com>
2025-03-16 12:26:52 -0300

    Deasafio-3-Criando-Board-com-Java

---
+++ src/test/java/Desafio_3_Criando_board_Com_Java/Desafio_3_Criando_board_Com_Java/Desafio3CriandoBoardComJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package Desafio_3_Criando_board_Com_Java.Desafio_3_Criando_board_Com_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio3CriandoBoardComJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
